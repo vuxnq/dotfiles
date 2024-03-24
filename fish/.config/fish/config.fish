30a9f9990faf282a201fe9ff5783feb20631874a
vuong <vuxnqu@gmail.com>
2024-03-24 01:11:29 +0100

    (init) added fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -Ux FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+
+set -gx EDITOR hx
+set -gx PATH $PATH ~/.spicetify # spicetify
+set -gx PATH $PATH ~/.cargo/bin # rust cargo
+set fish_greeting
+
+zoxide init --cmd cd fish | source
---
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -0,0 +1,14 @@
+function fish_greeting
+    switch (random 1 5)
+        case 1
+            echo "🐟"
+        case 2
+            echo "🐠"
+        case 3
+            echo "🐡"
+        case 4
+            echo "🐳"
+        case 5
+            echo "🦈"
+    end
+end
