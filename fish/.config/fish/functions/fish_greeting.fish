130198cfb6aeb8e11272bc029b0e101561dc2e22
vuong <vuxnqu@gmail.com>
2024-04-25 01:51:11 +0200

    big themes changes
    (themes) updated to gnome 46

--- fish/.config/fish/functions/fish_greeting.fish
+++ fish/.config/fish/functions/fish_greeting.fish
@@ -1,14 +1,3 @@
 function fish_greeting
-    switch (random 1 5)
-        case 1
-            echo "🐟"
-        case 2
-            echo "🐠"
-        case 3
-            echo "🐡"
-        case 4
-            echo "🐳"
-        case 5
-            echo "🦈"
-    end
+    echo hello, $(whoami)!
 end
