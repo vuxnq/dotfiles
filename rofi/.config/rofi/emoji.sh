9aeb8904ff77a06d44573fac70c98a7e7f0ee300
vuong <vuxnqu@gmail.com>
2024-04-29 21:30:05 +0200

    media controls

--- rofi/.config/rofi/emoji.sh
+++ rofi/.config/rofi/emoji.sh
@@ -4,4 +4,4 @@ rofimoji --selector-args=" \
     -theme-str 'configuration {show-icons: false; display-emoji: \"\";}' \
     -theme-str 'inputbar {children: [ "textbox-prompt-colon", "entry" ];}' \
     -theme-str 'listview {columns: 3;}' \
-    -theme ~/.config/rofi/config/launcher.rasi"
+    -theme $HOME/.config/rofi/config/launcher.rasi"
--- rofi/.config/rofi/volume.sh
+++ rofi/.config/rofi/volume.sh
@@ -1,111 +1,18 @@
 #!/usr/bin/env bash
 
-theme="$HOME/.config/rofi/config/applets.rasi"
+all_sinks=$(pactl list short sinks | cut -f 2)
 
-# Volume Info
-mixer="`amixer info Master | grep 'Mixer name' | cut -d':' -f2 | tr -d \',' '`"
-speaker="`amixer get Master | tail -n1 | awk -F ' ' '{print $5}' | tr -d '[]'`"
-mic="`amixer get Capture | tail -n1 | awk -F ' ' '{print $5}' | tr -d '[]'`"
+default_sink=$(pactl info | grep 'Default Sink' | cut -d : -f 2)
 
-active=""
-urgent=""
+active_sink=$(echo "$all_sinks" | grep -n $default_sink | cut -d : -f 1)
 
-# Speaker Info
-amixer get Master | grep '\[on\]' &>/dev/null
-if [[ "$?" == 0 ]]; then
-	active="-a 1"
-	stext='Unmute'
-	sicon=''
-else
-	urgent="-u 1"
-	stext='Mute'
-	sicon=''
-fi
+selected_sink=$(echo "$all_sinks" | rofi -dmenu -i -a $(($active_sink - 1)) \
+    -theme-str configuration{show-icons:false\;} \
+    -theme-str mainbox\{children:["message","listview"]\;} \
+    -theme-str window{width:300\;location:northeast\;anchor:northeast\;} \
+    -theme-str window{x-offset:-10px\;y-offset:10px\;} \
+    -theme-str listview{lines:4\;} \
+    -theme $HOME/.config/rofi/config/launcher.rasi
+)
 
-# Microphone Info
-amixer get Capture | grep '\[on\]' &>/dev/null
-if [[ "$?" == 0 ]]; then
-    [ -n "$active" ] && active+=",3" || active="-a 3"
-	mtext='Unmute'
-	micon=''
-else
-    [ -n "$urgent" ] && urgent+=",3" || urgent="-u 3"
-	mtext='Mute'
-	micon=''
-fi
-
-# Theme Elements
-prompt="S:$stext, M:$mtext"
-mesg="$mixer - Speaker: $speaker, Mic: $mic"
-
-list_col='1'
-list_row='5'
-
-# Options
-layout=`cat ${theme} | grep 'USE_ICON' | cut -d'=' -f2`
-if [[ "$layout" == 'NO' ]]; then
-	option_1=" Increase"
-	option_2="$sicon $stext"
-	option_3=" Decrese"
-	option_4="$micon $mtext"
-	option_5=" Settings"
-else
-	option_1=""
-	option_2="$sicon"
-	option_3=""
-	option_4="$micon"
-	option_5=""
-fi
-
-# Rofi CMD
-rofi_cmd() {
-	rofi -theme-str "listview {columns: $list_col; lines: $list_row;}" \
-		-theme-str 'textbox-prompt-colon {str: "";}' \
-		-dmenu \
-		-p "$prompt" \
-		-mesg "$mesg" \
-		${active} ${urgent} \
-		-markup-rows \
-		-theme ${theme}
-}
-
-# Pass variables to rofi dmenu
-run_rofi() {
-	echo -e "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | rofi_cmd
-}
-
-# Execute Command
-run_cmd() {
-	if [[ "$1" == '--opt1' ]]; then
-		amixer -Mq set Master,0 5%+ unmute
-	elif [[ "$1" == '--opt2' ]]; then
-		amixer set Master toggle
-	elif [[ "$1" == '--opt3' ]]; then
-		amixer -Mq set Master,0 5%- unmute
-	elif [[ "$1" == '--opt4' ]]; then
-		amixer set Capture toggle
-	elif [[ "$1" == '--opt5' ]]; then
-		pavucontrol
-	fi
-}
-
-# Actions
-chosen="$(run_rofi)"
-case ${chosen} in
-    $option_1)
-		run_cmd --opt1
-        ;;
-    $option_2)
-		run_cmd --opt2
-        ;;
-    $option_3)
-		run_cmd --opt3
-        ;;
-    $option_4)
-		run_cmd --opt4
-        ;;
-    $option_5)
-		run_cmd --opt5
-        ;;
-esac
-
+pactl set-default-sink $selected_sink
