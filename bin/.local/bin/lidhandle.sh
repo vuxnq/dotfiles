0027fb107de6439cefeceb3e284d553bd0b023b8
vuong <vuxnqu@gmail.com>
2024-05-05 15:37:47 +0200

    lidhandle.sh fix

--- bin/.local/bin/lidhandle.sh
+++ bin/.local/bin/lidhandle.sh
@@ -2,10 +2,10 @@
 
 state=$(cat /proc/acpi/button/lid/LID/state | tr -s ' ' | cut -d ' ' -f 2)
 
-if [ "$state" == "open" ]; then
-    hyprctl keyword monitor "eDP-1, preferred, 0x0, auto"
-else
-    count_monitors=$(hyprctl monitors | grep -c '^Monitor')
+if [ "$state" == "closed" ]; then
     hyprctl keyword monitor "eDP-1, disable"
-    [ "$count_monitors" = 1 ] && systemctl suspend
+    count=$(hyprctl monitors | grep -c '^Monitor')
+    [ "$count" = 0 ] && systemctl suspend
+else
+    hyprctl keyword monitor "eDP-1, preferred, 0x0, auto"
 fi
