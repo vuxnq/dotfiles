e7583b5022ed1807c588a1c3ec8b29abaf5d7e6d
vuong <vuxnqu@gmail.com>
2024-05-09 02:27:07 +0200

    removed hyprspace & added mic control waybar

--- rofi/.config/rofi/volume.sh
+++ rofi/.config/rofi/volume.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 
-all_sinks=$(pactl list short sinks | cut -f 2)
+# TODO: BRUH CLEANUP
 
-default_sink=$(pactl info | grep 'Default Sink' | cut -d : -f 2)
+if [ -n "$1" ]; then
+    all_sources=$(pactl list short sources | cut -f 2)
+    default_source=$(pactl get-default-source)
+    active_source=$(echo "$all_sources" | grep -n $default_source | cut -d : -f 1)
 
-active_sink=$(echo "$all_sinks" | grep -n $default_sink | cut -d : -f 1)
+    selected_source=$(echo "$all_sources" | rofi -dmenu -i -a $(($active_source - 1)) \
+        -theme-str configuration{show-icons:false\;} \
+        -theme-str mainbox\{children:["message","listview"]\;} \
+        -theme-str window{width:300\;location:northeast\;anchor:northeast\;} \
+        -theme-str window{x-offset:-10px\;y-offset:10px\;} \
+        -theme-str listview{lines:4\;} \
+        -theme $HOME/.config/rofi/config/launcher.rasi
+    )
+    pactl set-default-source $selected_source
+else
+    all_sinks=$(pactl list short sinks | cut -f 2)
+    default_sink=$(pactl get-default-sink)
+    active_sink=$(echo "$all_sinks" | grep -n $default_sink | cut -d : -f 1)
 
-selected_sink=$(echo "$all_sinks" | rofi -dmenu -i -a $(($active_sink - 1)) \
-    -theme-str configuration{show-icons:false\;} \
-    -theme-str mainbox\{children:["message","listview"]\;} \
-    -theme-str window{width:300\;location:northeast\;anchor:northeast\;} \
-    -theme-str window{x-offset:-10px\;y-offset:10px\;} \
-    -theme-str listview{lines:4\;} \
-    -theme $HOME/.config/rofi/config/launcher.rasi
-)
-
-pactl set-default-sink $selected_sink
+    selected_sink=$(echo "$all_sinks" | rofi -dmenu -i -a $(($active_sink - 1)) \
+        -theme-str configuration{show-icons:false\;} \
+        -theme-str mainbox\{children:["message","listview"]\;} \
+        -theme-str window{width:300\;location:northeast\;anchor:northeast\;} \
+        -theme-str window{x-offset:-10px\;y-offset:10px\;} \
+        -theme-str listview{lines:4\;} \
+        -theme $HOME/.config/rofi/config/launcher.rasi
+    )
+    pactl set-default-sink $selected_sink
+fi
