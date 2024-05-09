30f30d25447c9b37e26b3471b72df5bd3baf0ea3
vuong <vuxnqu@gmail.com>
2024-05-09 21:25:30 +0200

    waybar & wallpaper

--- waybar/.config/waybar/modules/spotify.sh
+++ waybar/.config/waybar/modules/spotify.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 playerctl -F metadata --player=spotify --format \
-    '{"text": "{{markup_escape(trunc(title, 40))}} | {{markup_escape(trunc(artist, 20))}}", "class": "custom-spotify", "alt": "{{lc(status)}}"}'
+    '{"text": "{{markup_escape(trunc(title, 40))}} | {{markup_escape(trunc(artist, 30))}}", "class": "custom-spotify", "alt": "{{lc(status)}}"}'
