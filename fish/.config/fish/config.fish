ecb09285d022e5f4522de833f9c9b1b83cd3a0f9
vuxnq <vuxnqu@gmail.com>
2024-10-02 00:00:42 +0200

    neovim stinky

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,7 +7,8 @@ set -Ux FZF_DEFAULT_OPTS "\
 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
 
-set -gx EDITOR hx
+set -gx EDITOR nvim
+
 set -gx PATH $PATH ~/.local/bin
 set -gx PATH $PATH ~/.spicetify # spicetify
 set -gx PATH $PATH ~/.cargo/bin # rust cargo
