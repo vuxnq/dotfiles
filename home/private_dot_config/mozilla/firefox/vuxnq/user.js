949654d9c9d6f553ed21da9a496f2899141ed23c
vuxnq <vuxnqu@gmail.com>
2026-03-17 02:04:52 +0100

    firefox celou dobu nemusel byt ve scriptu

---
+++ home/private_dot_config/mozilla/firefox/vuxnq/user.js
@@ -0,0 +1,4 @@
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("sidebar.verticalTabs", true);
+user_pref("browser.startup.homepage", "about:home");
+user_pref("general.autoScroll", true);
