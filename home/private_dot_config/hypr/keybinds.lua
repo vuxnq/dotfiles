d87f5e6df41519c6cce7361fd40939485c6bbe3a
vuxnq <vuxnqu@gmail.com>
2026-09-06 01:56:38 +0200

    proc hyprland nema split monitor workspaces

--- home/private_dot_config/hypr/keybinds.lua
+++ home/private_dot_config/hypr/keybinds.lua
@@ -75,11 +75,11 @@ for i = 1, 10 do
     local key = i % 10
     local czKey = vars.czDigits[i]
 
-    hl.bind(mainMod .. " + " .. czKey, hl.dsp.focus({ workspace = i }))
-    hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = i }))
+    hl.bind(mainMod .. " + " .. czKey, hl.dsp.focus({ workspace = "r~" .. i }))
+    hl.bind(mainMod .. " + " .. key, hl.dsp.focus({ workspace = "r~" .. i }))
 
-    hl.bind(mainMod .. " + SHIFT + " .. czKey, hl.dsp.window.move({ workspace = i }))
-    hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = i }))
+    hl.bind(mainMod .. " + SHIFT + " .. czKey, hl.dsp.window.move({ workspace = "r~" .. i }))
+    hl.bind(mainMod .. " + SHIFT + " .. key, hl.dsp.window.move({ workspace = "r~" .. i }))
 end
 
 -- special workspace
